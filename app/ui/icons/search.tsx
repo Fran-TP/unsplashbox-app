579c67fb1f31ef07c8e5477c5fe8081369a1c3fb
Fran-TP <ftorresp@unasam.edu.pe>
2024-12-09 02:25:44 -0500

    Update SearchIcon component to use currentColor for stroke properties

--- app/ui/icons/search.tsx
+++ app/ui/icons/search.tsx
@@ -8,13 +8,14 @@ const SearchIcon = (props: SVGProps<SVGSVGElement>) => {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
       {...props}
     >
       <title>search</title>
-      <circle cx="11" cy="11" r="7" stroke="#E5E7EB" strokeWidth="2" />
+      <circle cx="11" cy="11" r="7" stroke="currentColor" strokeWidth="2" />
       <path
         d="M20 20L17 17"
-        stroke="#E5E7EB"
+        stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
       />
