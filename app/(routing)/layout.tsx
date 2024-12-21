9d25e9333eb079056edfec4ee3ee59fd92fcef61
Fran-TP <ftorresp@unasam.edu.pe>
2024-12-20 21:27:52 -0500

    Remove unnecessary overflow properties from layout component for cleaner structure

--- app/(routing)/layout.tsx
+++ app/(routing)/layout.tsx
@@ -6,11 +6,11 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="h-dvh flex flex-col md:overflow-hidden">
+    <div className="h-dvh flex flex-col">
       <header className="p-4 md:pl-10 md:pr-12 md:py-[1.15rem] border-b border-b-light/80 dark:border-b-gray-700">
         <Nav />
       </header>
-      <main className="flex-grow  md:overflow-y-auto">{children}</main>
+      <main className="flex-grow">{children}</main>
     </div>
   )
 }
