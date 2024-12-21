f87a29f05ed0ad510227754dda3d2da604cef564
Fran-TP <ftorresp@unasam.edu.pe>
2024-12-20 21:28:09 -0500

    Implement photo fetching and display in GalleryPage with responsive layout

--- app/(routing)/collections/page.tsx
+++ app/(routing)/collections/page.tsx
@@ -1,7 +1,29 @@
-const GalleryPage = () => {
+import { fetchPhotos } from '@/app/lib/data'
+import Image from 'next/image'
+
+const GalleryPage = async () => {
+  const gallery = await fetchPhotos({ perPage: 40 })
+
   return (
-    <div>
-      <h1>Collection Page</h1>
+    <div className="relative">
+      <div className="bg-gradient-to-r from-red-300 from-60% via-indigo-500 via-80% to-purple-600 to-95% h-16" />
+      <section className="columns-4 p-14 gap-x-7 gap-y-7">
+        {gallery.map(image => {
+          return (
+            <article
+              key={image.id}
+              className="w-auto rounded-md overflow-clip mb-7"
+            >
+              <Image
+                src={image.urls.regular}
+                alt={image.alt_description}
+                width={image.width}
+                height={image.height}
+              />
+            </article>
+          )
+        })}
+      </section>
     </div>
   )
 }
