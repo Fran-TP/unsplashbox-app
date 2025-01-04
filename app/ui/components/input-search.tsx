e95343330ddece6c0e06da56883e8ab3b886616d
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-03 23:12:29 -0500

    Implement search functionality in InputSearch component

--- app/ui/components/input-search.tsx
+++ app/ui/components/input-search.tsx
@@ -1,6 +1,23 @@
+'use client'
+
 import SearchIcon from '@/ui/icons/search'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 const InputSearch = () => {
+  const searchParams = useSearchParams()
+  const pathName = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+
+    const params = new URLSearchParams(searchParams)
+
+    params.set('query', e.target.value)
+
+    replace(`${pathName}?${params.toString()}`)
+  }
+
   return (
     <div className="absolute -top-5 left-0 right-0 z-10 w-fit mt-14 mx-auto">
       <label htmlFor="search" className="sr-only">
@@ -12,6 +29,7 @@ const InputSearch = () => {
         placeholder="Enter your keywords..."
         autoComplete="off"
         className="peer border bg-gray-800 outline-none w-[22rem] sm:w-[30rem] lg:w-[35rem] xl:w-[39.5rem] border-gray-700 rounded-lg py-4 pl-4 pr-10 placeholder:text-gray-500 focus:text-light/80"
+        onChange={handleSearch}
       />
       <SearchIcon className="absolute text-balance top-1/2 right-5 -translate-y-1/2 size-7 text-gray-700 peer-focus:text-light/80 transition-colors duration-150 ease-in" />
     </div>
