057c11c49f86ef9c0d793b41bb5ecbaae5891937
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-03 23:18:49 -0500

    Add useDebounce hook for debouncing function calls

---
+++ app/hooks/useDebounce.tsx
@@ -0,0 +1,20 @@
+import { useRef } from 'react'
+
+export const useDebounce = (
+  callback: (value: string) => void,
+  delay: number
+) => {
+  const timeRef = useRef<number | null>(null)
+
+  const debounce = (param: string) => {
+    if (timeRef.current) {
+      clearTimeout(timeRef.current)
+    }
+
+    timeRef.current = window.setTimeout(() => {
+      callback(param)
+    }, delay)
+  }
+
+  return debounce
+}
