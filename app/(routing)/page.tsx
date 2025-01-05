8ebd44b9bc8280b518ede84c6577875228d46c7a
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-04 23:45:19 -0500

    Refactor search input to use InputSearch component for improved structure

--- app/(routing)/page.tsx
+++ app/(routing)/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import SearchIcon from '../ui/icons/search'
+import InputSearch from '../ui/components/input-search'
 
 const Home = () => {
   return (
@@ -16,19 +17,7 @@ const Home = () => {
         <p className="font-light mb-3">
           Search high-resolution images from Unsplash
         </p>
-        <div className="relative flex w-full">
-          <label htmlFor="search" className="sr-only">
-            Search
-          </label>
-          <input
-            id="search"
-            type="text"
-            placeholder="Enter your keywords..."
-            autoComplete="off"
-            className="peer border bg-gray-800 outline-none w-[22rem] sm:w-[30rem] lg:w-[35rem] xl:w-[39.5rem] border-gray-700 rounded-lg py-4 pl-4 pr-10 placeholder:text-gray-500 focus:text-light/80"
-          />
-          <SearchIcon className="absolute text-balance top-1/2 right-5 -translate-y-1/2 size-7 text-gray-700 peer-focus:text-light/80 transition-colors duration-150 ease-in" />
-        </div>
+        <InputSearch className="relative w-full" />
       </section>
       <Image
         src="/hero-right.webp"
