37b54e913e18b6108eb96fec9555ef991830597e
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-04 23:45:27 -0500

    Refactor GalleryPage component to handle async search parameters and enhance InputSearch integration

--- app/(routing)/collections/page.tsx
+++ app/(routing)/collections/page.tsx
@@ -3,11 +3,22 @@ import { GallerySkeleton } from '@/app/ui/components/skeletons'
 import PhotoGallery from '@/ui/collections/photo-gallery'
 import { Suspense } from 'react'
 
-const GalleryPage = () => {
+interface SearchParams {
+  query?: string
+}
+interface GalleryPageProps {
+  searchParams: Promise<SearchParams>
+}
+
+const GalleryPage = async ({ searchParams }: GalleryPageProps) => {
+  const collectionSearchParams = await searchParams
+
+  console.log(collectionSearchParams)
+
   return (
     <div className="relative">
       <div className="bg-gradient-to-r from-red-300 from-60% via-indigo-500 via-80% to-purple-600 to-95% h-16" />
-      <InputSearch />
+      <InputSearch className="absolute -top-5 left-0 right-0 z-10 w-fit mt-14 mx-auto" />
       <Suspense fallback={<GallerySkeleton />}>
         <PhotoGallery />
       </Suspense>
