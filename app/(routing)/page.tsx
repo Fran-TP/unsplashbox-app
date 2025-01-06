bb010f835c471ad2ed42630dff90d66d113afccd
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-05 22:15:06 -0500

    Implement search functionality in Home component with form submission and routing

--- app/(routing)/page.tsx
+++ app/(routing)/page.tsx
@@ -1,7 +1,20 @@
+'use client'
+
 import Image from 'next/image'
-import InputSearch from '../ui/components/input-search'
+import SearchIcon from '@/ui/icons/search'
+import { useRouter } from 'next/navigation'
 
 const Home = () => {
+  const { push } = useRouter()
+
+  const handleFormSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+
+    const data = new FormData(evt.currentTarget)
+
+    push(`/collections?query=${data.get('search')}`)
+  }
+
   return (
     <div className="relative h-full flex justify-center items-center overflow-x-clip">
       <Image
@@ -16,7 +29,26 @@ const Home = () => {
         <p className="font-light mb-3">
           Search high-resolution images from Unsplash
         </p>
-        <InputSearch className="relative w-full" />
+        <form onSubmit={handleFormSubmit} className="relative w-full">
+          <label htmlFor="search" className="sr-only">
+            Search
+          </label>
+          <input
+            id="search"
+            type="text"
+            name="search"
+            placeholder="Enter your keywords..."
+            autoComplete="off"
+            className="peer border-2 bg-gray-800 outline-0 w-[22rem] sm:w-[30rem] lg:w-[35rem] xl:w-[39.5rem] border-gray-700 rounded-xl py-4 pl-4 pr-10 placeholder:text-gray-500 focus:text-light/80"
+          />
+          <button
+            type="submit"
+            className="group absolute top-1/2 right-[0.60rem] -translate-y-1/2 text-gray-700 peer-focus:text-light/80 border-2 border-gray-700 p-1 rounded-md"
+            aria-describedby="search"
+          >
+            <SearchIcon className="size-7 text-inherit transition-colors duration-150 ease-in" />
+          </button>
+        </form>
       </section>
       <Image
         src="/hero-right.webp"
