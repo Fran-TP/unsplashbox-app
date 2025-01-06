fa1f8b26fe28ed6d1c78ea20585d7042630de947
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-05 23:45:51 -0500

    Enhance PhotoGallery component to handle search parameters and display a message when no photos are found

--- app/ui/collections/photo-gallery.tsx
+++ app/ui/collections/photo-gallery.tsx
@@ -1,25 +1,44 @@
 import { fetchUnsplashPhotos } from '@/app/lib/data/fetchPhotos'
-import { OrderBy } from 'unsplash-js'
 import PhotoCard from './photo-card'
+import clsx from 'clsx'
 
-const PhotoGallery = async () => {
+interface PhotoGalleryProps {
+  searchParams: Promise<{ query?: string }>
+}
+
+const PhotoGallery = async ({ searchParams }: PhotoGalleryProps) => {
+  const { query = 'cats' } = await searchParams
+
   const { results: photoCollection } = await fetchUnsplashPhotos({
+    query,
     perPage: 15,
     page: 1,
-    orderBy: OrderBy.DOWNLOADS
+    orderBy: 'relevant'
   })
 
+  console.log(photoCollection)
+
   return (
-    <section className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 p-14 gap-x-7">
-      {photoCollection.map(image => (
-        <PhotoCard
-          key={image.id}
-          urls={image.urls}
-          altDescription={image.alt_description ?? 'Unsplash Image'}
-          width={image.width}
-          height={image.height}
-        />
-      ))}
+    <section
+      className={clsx('p-14', {
+        'columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-x-7':
+          photoCollection.length > 0,
+        'grid place-items-center h-[80vh]': photoCollection.length === 0
+      })}
+    >
+      {photoCollection.length > 0 ? (
+        photoCollection.map(image => (
+          <PhotoCard
+            key={image.id}
+            urls={image.urls}
+            altDescription={image.alt_description ?? 'Unsplash Image'}
+            width={image.width}
+            height={image.height}
+          />
+        ))
+      ) : (
+        <p className="text-center text-lg text-light/80">No photos found</p>
+      )}
     </section>
   )
 }
