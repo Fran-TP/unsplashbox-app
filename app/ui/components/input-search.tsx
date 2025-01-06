00c1cad3c6bc510be08549c58b7c4dffd9a304a2
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-05 22:15:23 -0500

    Add default search text to InputSearch component from URL query parameters

--- app/ui/components/input-search.tsx
+++ app/ui/components/input-search.tsx
@@ -15,6 +15,8 @@ const InputSearch = ({ className }: InputSearchProps) => {
   const pathName = usePathname()
   const { replace } = useRouter()
 
+  const defaultSearchText = searchParams.get('query')?.toString()
+
   const handleSearch = useDebounce((value: string) => {
     const params = new URLSearchParams(searchParams)
 
@@ -39,6 +41,7 @@ const InputSearch = ({ className }: InputSearchProps) => {
         autoComplete="off"
         className="peer border bg-gray-800 outline-none w-[22rem] sm:w-[30rem] lg:w-[35rem] xl:w-[39.5rem] border-gray-700 rounded-lg py-4 pl-4 pr-10 placeholder:text-gray-500 focus:text-light/80"
         onChange={e => handleSearch(e.target.value)}
+        defaultValue={defaultSearchText}
       />
       <SearchIcon className="absolute text-balance top-1/2 right-5 -translate-y-1/2 size-7 text-gray-700 peer-focus:text-light/80 transition-colors duration-150 ease-in" />
     </div>
