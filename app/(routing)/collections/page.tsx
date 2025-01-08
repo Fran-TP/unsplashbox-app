bca63d8e6d37559cefb8d2d801dcd20a15e9f906
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-07 23:44:13 -0500

    Fetch photos based on search parameters in GalleryPage and update PhotoGallery component to use the photo collection promise

--- app/(routing)/collections/page.tsx
+++ app/(routing)/collections/page.tsx
@@ -1,3 +1,4 @@
+import { fetchUnsplashPhotos } from '@/app/lib/data/fetchPhotos'
 import InputSearch from '@/app/ui/components/input-search'
 import { GallerySkeleton } from '@/app/ui/components/skeletons'
 import PhotoGallery from '@/ui/collections/photo-gallery'
@@ -11,12 +12,20 @@ interface GalleryPageProps {
 }
 
 const GalleryPage = async ({ searchParams }: GalleryPageProps) => {
+  const { query = 'cats' } = await searchParams
+
+  const photoCollectionPromise = fetchUnsplashPhotos({
+    query,
+    perPage: 15,
+    orderBy: 'relevant'
+  })
+
   return (
-    <div className="relative flex flex-col">
+    <div className="relative flex flex-col h-full">
       <div className="flex-shrink-0 bg-gradient-to-r from-red-300 from-60% via-indigo-500 via-80% to-purple-600 to-95% h-16" />
-      <InputSearch className="absolute -top-5 left-0 right-0 z-10 w-fit mt-14 mx-auto" />
-      <Suspense fallback={<GallerySkeleton />}>
-        <PhotoGallery searchParams={searchParams} />
+      <InputSearch />
+      <Suspense key={query} fallback={<GallerySkeleton />}>
+        <PhotoGallery photoCollectionPromise={photoCollectionPromise} />
       </Suspense>
     </div>
   )
