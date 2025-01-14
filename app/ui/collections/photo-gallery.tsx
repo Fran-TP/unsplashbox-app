e9b6f547d69c6f46eac1b9e1b72f0f449409e745
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-14 00:24:20 -0500

    Refactor PhotoGallery to use useGallery hook for state management and simplify photo fetching logic

--- app/ui/collections/photo-gallery.tsx
+++ app/ui/collections/photo-gallery.tsx
@@ -1,21 +1,15 @@
 'use client'
 
-import {
-  fetchUnsplashPhotos,
-  type PhotoListResponse
-} from '@/app/lib/data/fetchPhotos'
 import PhotoCard from './photo-card'
 import clsx from 'clsx'
-import { useEffect, useState } from 'react'
 import { GallerySkeleton } from '../components/skeletons'
 import Masonry from 'react-layout-masonry'
+import { useGallery } from '@/app/hooks/useGallery'
 
 interface PhotoGalleryProps {
-  // photoCollectionPromise: Promise<PhotoListResponse>
   query: string
 }
 
-const INITIAL_PAGE = 1
 const ITEMS_GAP = 28
 const responsiveColumnCounts = {
   '320': 1,
@@ -25,37 +19,9 @@ const responsiveColumnCounts = {
 }
 
 const PhotoGallery = ({ query }: PhotoGalleryProps) => {
-  const [photos, setPhotos] = useState<PhotoListResponse['results']>([])
-  const [latestPhotoIds, setLatestPhotoIds] = useState<Set<string>>(new Set())
-  const [loading, setLoading] = useState(true)
-  const [totalPages, setTotalPages] = useState(0)
-  const [page, setPage] = useState(INITIAL_PAGE)
-
-  useEffect(() => {
-    setLoading(true)
-    fetchUnsplashPhotos({ perPage: 15, query, page })
-      .then(({ results, total_pages }) => {
-        const distinctPhotos = results.filter(
-          ({ id }) => !latestPhotoIds.has(id)
-        )
-
-        setLatestPhotoIds(new Set(results.map(photo => photo.id)))
-        setPhotos(photos.concat(distinctPhotos))
-        setTotalPages(total_pages)
-
-        setLoading(false)
-      })
-      .catch(() => {
-        console.error('Failed to fetch photos')
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [query, page])
-
-  const handleClick = () => {
-    setPage(page + 1)
-  }
+  const { photos, loading, page, totalPages, handleNextPage } = useGallery({
+    query
+  })
 
   return (
     <div className="flex flex-col h-full justify-center items-center pt-14 px-14 mb-7">
@@ -76,12 +42,15 @@ const PhotoGallery = ({ query }: PhotoGalleryProps) => {
           ))}
         </Masonry>
       )}
+      {photos.length === 0 && !loading && (
+        <p className="text-2xl text-gray-600">No photos found</p>
+      )}
       {loading && <GallerySkeleton />}
       {page < totalPages && (
         <button
           type="button"
           className="px-2 py-1 text-lg text-white bg-gray-800 border-2 border-gray-700 rounded-lg w-fit mb-2 mt-5"
-          onClick={handleClick}
+          onClick={handleNextPage}
         >
           Load More
         </button>
