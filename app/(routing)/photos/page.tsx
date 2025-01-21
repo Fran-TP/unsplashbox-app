2df75188ac983e79b9b81f4351b207d5ddd15dd4
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-21 03:02:11 -0500

    Add SearchPage component for photo search functionality with loading state

---
+++ app/(routing)/photos/page.tsx
@@ -0,0 +1,27 @@
+import InputSearch from '@/app/ui/components/input-search'
+import { GallerySkeleton } from '@/app/ui/components/skeletons'
+import PhotoGallery from '@/ui/collections/photo-gallery'
+import { Suspense } from 'react'
+
+interface SearchParams {
+  query?: string
+}
+interface SearchPageProps {
+  searchParams: Promise<SearchParams>
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { query = 'popular' } = await searchParams
+
+  return (
+    <div className="relative flex flex-col h-full">
+      <div className="flex-shrink-0 bg-gradient-to-r from-red-300 from-60% via-indigo-500 via-80% to-purple-600 to-95% h-16" />
+      <InputSearch />
+      <Suspense key={query} fallback={<GallerySkeleton />}>
+        <PhotoGallery query={query} />
+      </Suspense>
+    </div>
+  )
+}
+
+export default SearchPage
