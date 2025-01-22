6b802c3f22a059dfe3c1f982ec9372cbc9797708
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-21 23:54:43 -0500

    Enhance DetailPage component layout with header and photo ID display

--- app/(routing)/photos/[id]/page.tsx
+++ app/(routing)/photos/[id]/page.tsx
@@ -5,7 +5,12 @@ interface DetailPageProps {
 const DetailPage = async ({ params }: DetailPageProps) => {
   const { id } = await params
 
-  return <div>{id}</div>
+  return (
+    <div className="container">
+      <h1>Detail Page</h1>
+      <p>Photo ID: {id}</p>
+    </div>
+  )
 }
 
 export default DetailPage
