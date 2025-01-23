844a66a9bb1eef522b2d225f19c234ca0f908946
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-23 01:22:18 -0500

    Refactor fetchPhotoById to use updated import path for Unsplash API

--- app/lib/data/fetch-photo-by-id.ts
+++ app/lib/data/fetch-photo-by-id.ts
@@ -1,25 +1,7 @@
-import type { Basic } from 'unsplash-js/dist/methods/photos/types'
-import { unsplash } from './fetch-photos'
+'use server'
 
-export interface Photo {
-  id: string
-  description: string | null
-  urls: {
-    full: string
-    regular: string
-    small: string
-    thumb: string
-  }
-  user: {
-    name: string
-    username: string
-    profile_image: {
-      small: string
-      medium: string
-      large: string
-    }
-  }
-}
+import type { Basic } from 'unsplash-js/dist/methods/photos/types'
+import { unsplash } from '../constants'
 
 export const fetchPhotoById = async (id: string): Promise<Basic> => {
   const api = await unsplash.photos.get({ photoId: id })
