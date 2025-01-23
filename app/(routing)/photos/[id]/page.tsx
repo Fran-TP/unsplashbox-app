f3a8ae75ff47fd246ba5cf82bae707e5ac5893f7
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-22 23:54:25 -0500

    Add photo display to DetailPage using fetchPhotoById

--- app/(routing)/photos/[id]/page.tsx
+++ app/(routing)/photos/[id]/page.tsx
@@ -1,14 +1,26 @@
+import { fetchPhotoById } from '@/app/lib/data/fetchPhoto'
+import Image from 'next/image'
+
 interface DetailPageProps {
   params: Promise<{ id: string }>
 }
 
 const DetailPage = async ({ params }: DetailPageProps) => {
   const { id } = await params
+  const photo = await fetchPhotoById(id)
 
   return (
     <div className="container">
       <h1>Detail Page</h1>
       <p>Photo ID: {id}</p>
+      <div>
+        <Image
+          src={photo.urls.regular}
+          alt={photo.description ?? 'Untitled'}
+          width={photo.width}
+          height={photo.height}
+        />
+      </div>
     </div>
   )
 }
