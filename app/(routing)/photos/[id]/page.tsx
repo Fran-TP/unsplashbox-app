84b24eab71721b5918fd67a0b3f603b45ec73251
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-23 01:22:07 -0500

    Enhance photo detail page layout with user information and action buttons

--- app/(routing)/photos/[id]/page.tsx
+++ app/(routing)/photos/[id]/page.tsx
@@ -10,17 +10,44 @@ const DetailPage = async ({ params }: DetailPageProps) => {
   const photo = await fetchPhotoById(id)
 
   return (
-    <div className="container">
-      <h1>Detail Page</h1>
-      <p>Photo ID: {id}</p>
-      <div>
-        <Image
-          src={photo.urls.regular}
-          alt={photo.description ?? 'Untitled'}
-          width={photo.width}
-          height={photo.height}
-        />
-      </div>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 xl:grid-cols-2 lg:gap-8 p-14">
+      <Image
+        className="rounded-lg"
+        src={photo.urls.regular}
+        alt={photo.description ?? 'Untitled'}
+        width={photo.width}
+        height={photo.height}
+      />
+      <section className="flex flex-col gap-4">
+        <div className="flex items-center gap-4">
+          <Image
+            src={photo.user.profile_image.medium}
+            alt={photo.user.name}
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <p className="text-light/80 font-medium">{photo.user.name}</p>
+        </div>
+        <p>
+          Published on{' '}
+          <time>
+            {new Date(photo.created_at).toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric'
+            })}
+          </time>
+        </p>
+        <div className="flex gap-4">
+          <button type="button">Add to collection</button>
+          <button type="button">Download</button>
+        </div>
+        <section className="mt-6">
+          <h1 className="text-3xl text-light/80">Collection</h1>
+          {/* <div className="flex gap-4"></div> */}
+        </section>
+      </section>
     </div>
   )
 }
