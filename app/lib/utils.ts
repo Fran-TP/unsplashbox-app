03a3ed8b520db7a9c6dec3079335ab66f4d55c42
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-23 23:27:50 -0500

    Add formatDate utility function for improved date formatting

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,2 +1,15 @@
 export const sleep = (delay: number) =>
   new Promise(resolve => setTimeout(resolve, delay))
+
+export const formatDate = (inputDate: string, locale = navigator.language) => {
+  const date = new Date(inputDate)
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+
+  const formatter = new Intl.DateTimeFormat(locale, options)
+
+  return formatter.format(date)
+}
