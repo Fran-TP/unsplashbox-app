8e5d70e385ac9ad418a523991f55f64616429490
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-26 23:18:36 -0500

    Implement modal component with dismiss functionality and portal rendering

--- app/ui/photos/modal.tsx
+++ app/ui/photos/modal.tsx
@@ -1,12 +1,44 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
+
 interface ModalProps {
   children: React.ReactNode
 }
 
 const Modal = ({ children }: ModalProps) => {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-dark">
+  const router = useRouter()
+  const dialogRef = useRef<HTMLDialogElement | null>(null)
+
+  useEffect(() => {
+    if (!dialogRef?.current?.open) {
+      dialogRef?.current?.showModal()
+    }
+  }, [])
+
+  const handleDismiss = () => {
+    dialogRef.current?.close()
+
+    setTimeout(router.back, 310)
+  }
+
+  return createPortal(
+    <dialog
+      ref={dialogRef}
+      className="bg-dark backdrop:bg-[linear-gradient(45deg,var(--tw-gradient-stops))] backdrop:from-green-700/90 backdrop:to-transparent rounded-lg"
+    >
       {children}
-    </div>
+      <button
+        type="button"
+        onClick={handleDismiss}
+        className="absolute top-4 right-4 dark:text-light/80 text-dark/80"
+      >
+        ❌
+      </button>
+    </dialog>,
+    document.body
   )
 }
 
