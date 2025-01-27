f4996df407b13fab0ff7f14fa97b57962d497c44
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-26 23:19:45 -0500

    Remove unused sleep import from fetchPhotoById function

--- app/lib/data/fetch-photo-by-id.ts
+++ app/lib/data/fetch-photo-by-id.ts
@@ -1,6 +1,5 @@
 import type { Basic } from 'unsplash-js/dist/methods/photos/types'
 import { unsplash } from '../constants'
-import { sleep } from '../utils'
 
 export const fetchPhotoById = async (id: string): Promise<Basic> => {
   const api = await unsplash.photos.get({ photoId: id })
