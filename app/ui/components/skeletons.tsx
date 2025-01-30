0ee1dc1a31a1603586899cae9734068c3b59e5fa
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-29 22:57:48 -0500

    Add CollectionSkeleton component for loading state representation

--- app/ui/components/skeletons.tsx
+++ app/ui/components/skeletons.tsx
@@ -29,3 +29,30 @@ export const GallerySkeleton = () => (
     })}
   </div>
 )
+
+export const CollectionSkeleton = ({
+  rows: skeletonRows = 5
+}: { rows: number }) => {
+  return (
+    <div className="p-3">
+      {Array.from({ length: skeletonRows }).map((_, i) => {
+        const key = `skeleton-${i}`
+
+        return (
+          <li
+            key={key}
+            className="group flex items-center gap-4 snap-start hover:bg-gray-800 p-3 rounded-xl transition-colors duration-200 ease-in"
+          >
+            <div className="rounded-md overflow-hidden">
+              <PhotoIcon className="size-16 stroke-1" />
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+              <div className="w-full h-3 bg-gray-600 rounded-lg" />
+              <div className="w-14 h-2 bg-gray-600 rounded-lg" />
+            </div>
+          </li>
+        )
+      })}
+    </div>
+  )
+}
