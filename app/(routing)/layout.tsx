48bbbeec0eafb0bc7a39bc46618e089b958434ea
Fran-TP <ftorresp@unasam.edu.pe>
2025-01-31 23:52:13 -0500

    Refactor layout styles: enhance header and main background for improved visibility

--- app/(routing)/layout.tsx
+++ app/(routing)/layout.tsx
@@ -7,10 +7,10 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="h-dvh flex flex-col">
-      <header className="p-4 md:pl-10 md:pr-12 md:py-[1.15rem] border-b border-b-light/80 dark:border-b-gray-700">
+      <header className="p-4 md:pl-10 md:pr-12 md:py-[1.15rem] border-b-2 border-b-light dark:border-b-gray-700 bg-lighter dark:bg-dark">
         <Nav />
       </header>
-      <main className="grow">{children}</main>
+      <main className="grow bg-lighter dark:bg-dark">{children}</main>
     </div>
   )
 }
