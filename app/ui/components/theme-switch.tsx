f45a738a9e4d672dd3160538f1cbeeea2e43393e
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-01 20:31:30 -0500

    Add SwitchTheme component: implement theme switching functionality

---
+++ app/ui/components/theme-switch.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+
+const SwitchTheme = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+
+  return (
+    <select
+      value={theme}
+      onChange={e => setTheme(e.target.value)}
+      className="text-dark dark:text-lighter bg-lighter dark:bg-dark"
+    >
+      <option value="system">System</option>
+      <option value="dark">Dark</option>
+      <option value="light">Light</option>
+    </select>
+  )
+}
+
+export default SwitchTheme
