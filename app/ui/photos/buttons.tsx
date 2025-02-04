713d2ed5e0a2ef819b76b3598431d4029dc53240
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-03 22:33:01 -0500

    Enhance button components: improve layout and accessibility for Add and Download actions

--- app/ui/photos/buttons.tsx
+++ app/ui/photos/buttons.tsx
@@ -8,34 +8,36 @@ export const AddPhoto = () => {
   return (
     <button
       type="button"
-      className="flex items-center justify-between gap-2 px-6 cursor-pointer text-dark/90 dark:text-light/80 bg-light dark:bg-gray-800 p-2 rounded-sm transition-colors duration-200 ease-in"
+      className="min-w-fit flex-1 flex items-center justify-center gap-2 px-6 cursor-pointer text-dark/90 dark:hover:bg-[#2563EB]/30 hover:bg-gray-300 dark:text-light/80 bg-light dark:bg-gray-800 p-2 rounded-sm transition-colors duration-200 ease-in"
     >
-      <PlusIcon className="size-5" /> Add to collection
+      <PlusIcon className="size-5" />
+      Add to collection
     </button>
   )
 }
 
 export const DownloadPhoto = ({ photo }: { photo: Basic }) => {
   const handleDownload = async () => {
-    const a = document.createElement('a')
+    const anchorTag = document.createElement('a')
     const response = await fetch(photo.urls.full)
     const blob = await response.blob()
     const url = window.URL.createObjectURL(blob)
 
-    a.href = url
-    a.download = photo.alt_description
+    anchorTag.href = url
+    anchorTag.download = photo.alt_description
       ? photo.alt_description.split(' ').join('-')
       : photo.id
-    a.click()
+    anchorTag.click()
   }
 
   return (
     <button
       type="button"
       onClick={handleDownload}
-      className="flex items-center justify-between gap-2 px-6 text-dark/90 bg-light cursor-pointer dark:text-light/80 dark:bg-gray-800 p-2 rounded-sm transition-colors duration-200 ease-in"
+      className="min-w-fit flex-1 flex items-center justify-center gap-2 px-6 text-dark/90 bg-light hover:bg-gray-300 dark:hover:bg-[#2563EB]/30 cursor-pointer dark:text-light/80 dark:bg-gray-800 p-2 rounded-sm transition-colors duration-200 ease-in"
     >
-      <DownloadIcon className="size-5" /> Download
+      <DownloadIcon className="size-5" />
+      Download
     </button>
   )
 }
